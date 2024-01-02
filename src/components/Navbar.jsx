aa315b6cab5fec5c3c8ab674359030fa9f020deb
Vatsal Motiani <54301562+vatsalmotiani@users.noreply.github.com>
2024-01-02 21:43:17 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 "use client";
 import Image from "next/image";
-
+import { useState } from "react";
 import Link from "next/link";
-import { useSelectedLayoutSegment } from "next/navigation";
 import { usePathname } from "next/navigation";
+import { Menu, X } from "react-feather";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 export default function Navbar() {
   const pathname = usePathname();
+
+  const [navOpen, setNavOpen] = useState(false);
   const navLinks = [
     {
       name: "About",
@@ -23,33 +26,77 @@ export default function Navbar() {
       url: "/contact-us",
     },
   ];
-  return (
-    <div className='flex w-5/6 justify-between items-center px-6 pt-8'>
-      <Link href='/'>
-        <Image
-          src='/tb-logo.svg'
-          width='137'
-          height='0'
-          sizes='100vw'
-          className='h-auto'
-          alt='Thought Bubbles Logo'
-        />
-      </Link>
 
-      <div className='text-base font-medium'>
-        {navLinks.map(({ name, url, parent }) => {
-          const isActive = pathname.startsWith(`${url}`) || pathname.startsWith(`${parent}`);
-          return (
-            <Link
-              key={url}
-              className={`px-8  ${isActive ? "text-tb-blue " : "text-tb-black hover:text-tb-body duration-300"}`}
-              href={url}
+  const handleNav = () => {
+    setNavOpen(!navOpen);
+  };
+
+  return (
+    <nav className='bg-white md:bg-transparent w-full'>
+      <div className='max-w-7xl mx-auto pb-4 md:pb-0 pt-4 px-4 sm:px-6 lg:px-8'>
+        <div className='flex items-center justify-between h-16'>
+          <div className='flex items-center '>
+            <div className='flex-shrink-0'>
+              <Link
+                href='/'
+                onClick={() => setNavOpen(false)}
+              >
+                <Image
+                  src='/tb-logo.svg'
+                  width='137'
+                  height='0'
+                  sizes='100vw'
+                  className='h-auto'
+                  alt='Thought Bubbles Logo'
+                />
+              </Link>
+            </div>
+          </div>
+          <div className='hidden md:block'>
+            <div className='ml-4 flex items-center space-x-4 '>
+              {navLinks.map(({ name, url, parent }) => {
+                const isActive = pathname.startsWith(`${url}`) || pathname.startsWith(`${parent}`);
+                return (
+                  <Link
+                    key={url}
+                    className={`p-2 ${isActive ? "text-tb-blue " : "text-tb-black hover:text-tb-body duration-300"}`}
+                    href={url}
+                  >
+                    {name}
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+          <div className='md:hidden flex items-center'>
+            <button
+              className='inline-flex items-center justify-center p-2 rounded-md'
+              onClick={handleNav}
             >
-              {name}
-            </Link>
-          );
-        })}
+              {navOpen ? <X /> : <Menu />}
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+      {navOpen && (
+        <div className='md:hidden bg-white '>
+          <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
+            {navLinks.map(({ name, url, parent }) => {
+              const isActive = pathname.startsWith(`${url}`) || pathname.startsWith(`${parent}`);
+              return (
+                <Link
+                  key={url}
+                  className={`p-2 block ${isActive ? "text-tb-blue " : "text-tb-black hover:text-tb-body duration-300"}`}
+                  href={url}
+                  onClick={handleNav}
+                >
+                  {name}
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </nav>
   );
 }
